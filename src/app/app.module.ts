bc986afef0d65f3e0ef804719ba8046098ee5b04
Vijay <Vijay>
2018-06-21 11:01:40 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HttpModule } from '@angular/http';
 import { CarService } from './shared/car/car.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,18 +10,48 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CarListComponent } from './car-list/car-list.component';
 
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GiphyService } from './shared/giphy/giphy.service';
+import { CarEditComponent } from './car-edit/car-edit.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/car-list', pathMatch: 'full' },
+  {
+    path: 'car-list',
+    component: CarListComponent
+  },
+  {
+    path: 'car-add',
+    component: CarEditComponent
+  },
+  {
+    path: 'car-edit/:id',
+    component: CarEditComponent
+  }
+];
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    CarListComponent
+    CarListComponent,
+    CarEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
-  providers: [CarService],
+  providers: [CarService, GiphyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
